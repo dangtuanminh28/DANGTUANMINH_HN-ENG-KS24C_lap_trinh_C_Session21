e159411b43d7ad5375e8b642ab3fc64df1917f24
Đặng Tuấn Minh <dangmanhcuong27011971@gmail.com>
2024-12-25 07:58:30 +0700

    Update Session21-1.c

--- Session21-1.c
+++ Session21-1.c
@@ -3,15 +3,13 @@
 int main() {
     FILE *fptr;
     char str[100];
-    fptr = fopen("bt01.txt", "w");
+    fptr = fopen("bt01.txt", "r");
     if (fptr==NULL) {
         printf("Loi mo file!\n");
         return 1;
     }
-    printf("Nhap mot chuoi bat ky: ");
-    fgets(str, sizeof(str), stdin);
-    fputs(str, fptr);
+	fgets(str, sizeof(str), fptr);
+	printf("Dong dau tien doc tu file: %s\n", str);
     fclose(fptr);
-    printf("Da ghi du lieu vao file bt01.txt thanh cong\n");
     return 0;
 }
